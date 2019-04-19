ddffd5ad18e3b0998a383d56e1c6601069fbf832
Nicholas Jamieson <nicholas@cartant.com>
2019-04-19 11:11:40 +1000

    chore: Use prettier.

--- source/utils.ts
+++ source/utils.ts
@@ -8,148 +8,160 @@ import * as tsutils from "tsutils";
 import { InternalSymbolName } from "typescript";
 
 export function couldBeFunction(type: ts.Type): boolean {
-    return (type.getCallSignatures().length > 0) ||
-        couldBeType(type, "Function") ||
-        couldBeType(type, "ArrowFunction") ||
-        couldBeType(type, InternalSymbolName.Function);
+  return (
+    type.getCallSignatures().length > 0 ||
+    couldBeType(type, "Function") ||
+    couldBeType(type, "ArrowFunction") ||
+    couldBeType(type, InternalSymbolName.Function)
+  );
 }
 
 export function couldBeType(
-    type: ts.Type,
-    name: string | RegExp,
-    qualified?: {
-        name: RegExp,
-        typeChecker: ts.TypeChecker
-    }
+  type: ts.Type,
+  name: string | RegExp,
+  qualified?: {
+    name: RegExp;
+    typeChecker: ts.TypeChecker;
+  }
 ): boolean {
+  if (isReferenceType(type)) {
+    type = type.target;
+  }
 
-    if (isReferenceType(type)) {
-        type = type.target;
-    }
+  if (isType(type, name, qualified)) {
+    return true;
+  }
 
-    if (isType(type, name, qualified)) {
-        return true;
-    }
+  if (isIntersectionType(type) || isUnionType(type)) {
+    return type.types.some(t => couldBeType(t, name, qualified));
+  }
 
-    if (isIntersectionType(type) || isUnionType(type)) {
-        return type.types.some((t) => couldBeType(t, name, qualified));
-    }
-
-    const baseTypes = type.getBaseTypes();
-    if (!baseTypes) {
-        return false;
-    }
-    return baseTypes.some((t) => couldBeType(t, name, qualified));
+  const baseTypes = type.getBaseTypes();
+  if (!baseTypes) {
+    return false;
+  }
+  return baseTypes.some(t => couldBeType(t, name, qualified));
 }
 
-export function findDeclaration(node: ts.Node, typeChecker: ts.TypeChecker): ts.Declaration | undefined {
-
-    const symbol = typeChecker.getSymbolAtLocation(node);
-    if (!symbol) {
-        return undefined;
-    }
-    const declarations = symbol.getDeclarations();
-    if (!declarations || (declarations.length === 0)) {
-        return undefined;
-    }
-    const [declaration] = declarations;
-    return declaration;
+export function findDeclaration(
+  node: ts.Node,
+  typeChecker: ts.TypeChecker
+): ts.Declaration | undefined {
+  const symbol = typeChecker.getSymbolAtLocation(node);
+  if (!symbol) {
+    return undefined;
+  }
+  const declarations = symbol.getDeclarations();
+  if (!declarations || declarations.length === 0) {
+    return undefined;
+  }
+  const [declaration] = declarations;
+  return declaration;
 }
 
 export function isAny(type: ts.Type): boolean {
-
-    return tsutils.isTypeFlagSet(type, ts.TypeFlags.Any);
+  return tsutils.isTypeFlagSet(type, ts.TypeFlags.Any);
 }
 
 export function isConstDeclaration(declaration: ts.Declaration): boolean {
+  let variableDeclarationList: ts.VariableDeclarationList | null = null;
 
-    let variableDeclarationList: ts.VariableDeclarationList | null = null;
-
-    if (tsutils.isVariableDeclaration(declaration)) {
-        if (tsutils.isVariableDeclarationList(declaration.parent)) {
-            variableDeclarationList = declaration.parent;
-        }
-    } else if (tsutils.isBindingElement(declaration)) {
-        let parent: ts.Node = declaration.parent;
-        while (tsutils.isBindingPattern(parent) || tsutils.isVariableDeclaration(parent)) {
-            parent = parent.parent;
-        }
-        if (tsutils.isVariableDeclarationList(parent)) {
-            variableDeclarationList = parent;
-        }
+  if (tsutils.isVariableDeclaration(declaration)) {
+    if (tsutils.isVariableDeclarationList(declaration.parent)) {
+      variableDeclarationList = declaration.parent;
     }
-
-    if (variableDeclarationList) {
-        return tsutils.getVariableDeclarationKind(variableDeclarationList) === tsutils.VariableDeclarationKind.Const;
+  } else if (tsutils.isBindingElement(declaration)) {
+    let parent: ts.Node = declaration.parent;
+    while (
+      tsutils.isBindingPattern(parent) ||
+      tsutils.isVariableDeclaration(parent)
+    ) {
+      parent = parent.parent;
     }
-    return false;
+    if (tsutils.isVariableDeclarationList(parent)) {
+      variableDeclarationList = parent;
+    }
+  }
+
+  if (variableDeclarationList) {
+    return (
+      tsutils.getVariableDeclarationKind(variableDeclarationList) ===
+      tsutils.VariableDeclarationKind.Const
+    );
+  }
+  return false;
 }
 
 export function isInstanceofCtor(node: ts.Node): boolean {
-
-    const { parent } = node;
-    return tsutils.isBinaryExpression(parent) &&
-        (node === parent.right) &&
-        (parent.operatorToken.kind === ts.SyntaxKind.InstanceOfKeyword);
+  const { parent } = node;
+  return (
+    tsutils.isBinaryExpression(parent) &&
+    node === parent.right &&
+    parent.operatorToken.kind === ts.SyntaxKind.InstanceOfKeyword
+  );
 }
 
 export function isIntersectionType(type: ts.Type): type is ts.UnionType {
-
-    return tsutils.isTypeFlagSet(type, ts.TypeFlags.Intersection);
+  return tsutils.isTypeFlagSet(type, ts.TypeFlags.Intersection);
 }
 
 export function isReferenceType(type: ts.Type): type is ts.TypeReference {
-
-    return tsutils.isTypeFlagSet(type, ts.TypeFlags.Object) &&
-        tsutils.isObjectFlagSet(type as ts.ObjectType, ts.ObjectFlags.Reference);
+  return (
+    tsutils.isTypeFlagSet(type, ts.TypeFlags.Object) &&
+    tsutils.isObjectFlagSet(type as ts.ObjectType, ts.ObjectFlags.Reference)
+  );
 }
 
 export function isThis(node: ts.Node): boolean {
-
-    return node.kind === ts.SyntaxKind.ThisKeyword;
+  return node.kind === ts.SyntaxKind.ThisKeyword;
 }
 
 export function isType(
-    type: ts.Type,
-    name: string | RegExp,
-    qualified?: {
-        name: RegExp,
-        typeChecker: ts.TypeChecker
-    }
+  type: ts.Type,
+  name: string | RegExp,
+  qualified?: {
+    name: RegExp;
+    typeChecker: ts.TypeChecker;
+  }
 ): boolean {
-
-    if (!type.symbol) {
-        return false;
-    }
-    if (qualified && !qualified.name.test(qualified.typeChecker.getFullyQualifiedName(type.symbol))) {
-        return false;
-    }
-    return (typeof name === "string") ?
-        (type.symbol.name === name) :
-        Boolean(type.symbol.name.match(name));
+  if (!type.symbol) {
+    return false;
+  }
+  if (
+    qualified &&
+    !qualified.name.test(
+      qualified.typeChecker.getFullyQualifiedName(type.symbol)
+    )
+  ) {
+    return false;
+  }
+  return typeof name === "string"
+    ? type.symbol.name === name
+    : Boolean(type.symbol.name.match(name));
 }
 
 export function isUnionType(type: ts.Type): type is ts.UnionType {
-
-    return tsutils.isTypeFlagSet(type, ts.TypeFlags.Union);
+  return tsutils.isTypeFlagSet(type, ts.TypeFlags.Union);
 }
 
 export function isWithinCallExpressionExpression(node: ts.Node): boolean {
-
-    let { parent } = node;
-    while (parent && tsutils.isPropertyAccessExpression(parent)) {
-        node = parent;
-        parent = node.parent;
-    }
-    return parent && tsutils.isCallExpression(parent) && (node === parent.expression);
+  let { parent } = node;
+  while (parent && tsutils.isPropertyAccessExpression(parent)) {
+    node = parent;
+    parent = node.parent;
+  }
+  return (
+    parent && tsutils.isCallExpression(parent) && node === parent.expression
+  );
 }
 
 export function isWithinParameterDeclaration(node: ts.Node): boolean {
-
-    if (tsutils.isParameterDeclaration(node)) {
-        return true;
-    }
-    return tsutils.isBindingElement(node) &&
-        tsutils.isBindingPattern(node.parent) &&
-        tsutils.isParameterDeclaration(node.parent.parent);
+  if (tsutils.isParameterDeclaration(node)) {
+    return true;
+  }
+  return (
+    tsutils.isBindingElement(node) &&
+    tsutils.isBindingPattern(node.parent) &&
+    tsutils.isParameterDeclaration(node.parent.parent)
+  );
 }
